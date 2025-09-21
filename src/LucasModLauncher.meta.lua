1bf26a1e22c102ffe64aacdd8162ac95ff06c663
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-09-21 16:43:48 -0400

    feat: add 1.27 global tables

--- src/LucasModLauncher.meta.lua
+++ src/LucasModLauncher.meta.lua
@@ -2,6 +2,161 @@
 
 error("Meta files should not be executed.")
 
+--
+-- Globals
+--
+
+Cheat =
+{
+	UnlockAllRewardVehicles = 32,
+	NoTopSpeed = 128,
+	HighAcceleration = 256,
+	CarJumpOnHorn = 512,
+	OneTapTrafficDeath = 2048,
+	KickSwapsCharacterModel = 16384,
+	PlayCreditsDialogue = 262144,
+	ShowSpeedometer = 524288,
+	RedBrick = 1048576,
+	InvincibleCar = 2097152,
+	ShowTree = 4194304,
+	Trippy = 8388608,
+}
+
+DialogButtons =
+{
+	OK = 0,
+	OKCancel = 1,
+	OKClose = 2,
+	YesNo = 3,
+	YesClose = 4,
+	YesNoCancel = 5,
+	YesNoClose = 6,
+	AbortRetryIgnore = 7,
+	CloseRetryIgnore = 8,
+	RetryCancel = 9,
+	RetryClose = 10,
+	Close = 11,
+}
+
+DialogIcon =
+{
+	Error = 0,
+	Warning = 1,
+	Information = 2,
+	None = 3,
+}
+
+DialogResult =
+{
+	OK = 0,
+	Cancel = 1,
+	Close = 2,
+	Yes = 3,
+	No = 4,
+	Abort = 5,
+	Retry = 6,
+	Ignore = 7,
+}
+
+PersistentObjectSector =
+{
+	L1R1 = 0,
+	L1R2 = 1,
+	L1R3 = 2,
+	L1R4A = 3,
+	L1R4B = 4,
+	L1R6 = 5,
+	L1R7 = 6,
+	L1Z1 = 7,
+	L1Z2 = 8,
+	L1Z3 = 9,
+	L1Z4 = 10,
+	L1Z6 = 11,
+	L1Z7 = 12,
+
+	L2R1 = 13,
+	L2R2 = 14,
+	L2R3 = 15,
+	L2R4 = 16,
+	L2Z1 = 17,
+	L2Z2 = 18,
+	L2Z3 = 19,
+	L2Z4 = 20,
+
+	L3R1 = 21,
+	L3R2 = 22,
+	L3R3 = 23,
+	L3R4 = 24,
+	L3R5 = 25,
+	L3Z1 = 26,
+	L3Z2 = 27,
+	L3Z3 = 28,
+	L3Z4 = 29,
+	L3Z5 = 30,
+
+	L4R1 = 31,
+	L4R2 = 32,
+	L4R3 = 33,
+	L4R4A = 34,
+	L4R4B = 35,
+	L4R6 = 36,
+	L4R7 = 37,
+	L4Z1 = 38,
+	L4Z2 = 39,
+	L4Z3 = 40,
+	L4Z4 = 41,
+	L4Z6 = 42,
+	L4Z7 = 43,
+
+	L5R1 = 44,
+	L5R2 = 45,
+	L5R3 = 46,
+	L5R4 = 47,
+	L5Z1 = 48,
+	L5Z2 = 49,
+	L5Z3 = 50,
+	L5Z4 = 51,
+
+	L6R1 = 52,
+	L6R2 = 53,
+	L6R3 = 54,
+	L6R4 = 55,
+	L6R5 = 56,
+	L6Z1 = 57,
+	L6Z2 = 58,
+	L6Z3 = 59,
+	L6Z4 = 60,
+	L6Z5 = 61,
+
+	L7R1 = 62,
+	L7R2 = 63,
+	L7R3 = 64,
+	L7R4A = 65,
+	L7R4B = 66,
+	L7R6 = 67,
+	L7R7 = 68,
+	L7Z1 = 69,
+	L7Z2 = 70,
+	L7Z3 = 71,
+	L7Z4 = 72,
+	L7Z6 = 73,
+	L7Z7 = 74,
+
+	Level1 = 75,
+	Level2 = 76,
+	Level3 = 77,
+	Level4 = 78,
+	Level5 = 79,
+	Level6 = 80,
+	Level7 = 81,
+}
+
+RewardType =
+{
+	Skin = 1,
+	Car = 3,
+}
+
 ---Shows a message box for debugging purposes with an "OK" button.
 ---
 ---@param Message string
